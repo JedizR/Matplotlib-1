6e33b49f65fa325eeb04c6ac9cf3e9289314acea
JedizR <teenday.jedi@gmail.com>
2025-01-24 01:57:09 +0700

    material added and plotted 3 functions

---
+++ matplotlib_1.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(-10, 10, 1000)
+
+y1 = x**2
+y2 = x * np.sin(2 * x)
+y3 = np.arctan(x)
+
+plt.plot(x, y1, label='x^2')
+plt.plot(x, y2, label='x * sin(2x)')
+plt.plot(x, y3, label='arctan(x)')
+
+plt.show()
