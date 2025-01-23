3a9acab564eea9098aaec1cbf73270681157edcc
JedizR <teenday.jedi@gmail.com>
2025-01-24 02:26:21 +0700

    linestyle added

--- matplotlib_1.py
+++ matplotlib_1.py
@@ -8,9 +8,9 @@ y2 = x * np.sin(2 * x)
 y3 = np.arctan(x)
 
 plt.plot(x, y1, label='x^2', color='green')
-plt.plot(x, y2, label='x * sin(2x)', color='red')
+plt.plot(x, y2, label='x * sin(2x)', color='red', linestyle='dashed')
 SUNSET_BOULEVARD_COLOR = (247/255,148/255,107/255)
-plt.plot(x, y3, label='arctan(x)' , color=SUNSET_BOULEVARD_COLOR)
+plt.plot(x, y3, label='arctan(x)' , color=SUNSET_BOULEVARD_COLOR, linestyle='dotted')
 
 plt.legend(loc='upper left')
 plt.title('Function Graphs')
