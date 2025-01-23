e2f3fa0da6ebdcfda219839538c1d958de6ef749
JedizR <teenday.jedi@gmail.com>
2025-01-24 02:02:51 +0700

    Added legend, plot title, axes labels

--- matplotlib_1.py
+++ matplotlib_1.py
@@ -11,4 +11,9 @@ plt.plot(x, y1, label='x^2')
 plt.plot(x, y2, label='x * sin(2x)')
 plt.plot(x, y3, label='arctan(x)')
 
+plt.legend(loc='upper left')
+plt.title('Function Graphs')
+plt.xlabel('x')
+plt.ylabel('y')
+
 plt.show()
